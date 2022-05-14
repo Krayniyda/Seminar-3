7d230cfcd4cf95bcc94bf3730ea8d8d5c20c5640
KrayniyDA <Krayniy19730507@gmail.com>
2022-05-15 05:02:47 +0700

    first commit

---
+++ Tasks3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N. ");
+Console.Write("Введите N (от 1 до 255): ");
+byte N = Convert.ToByte(Console.ReadLine());
+Console.WriteLine("");
+
+for (int i = 1; i <= N; ++i)
+{
+    string text = "--------+----------\n";
+    text = text + "   " + i + " \t| " + i * i * i;
+    Console.WriteLine(text);
+}
+Console.WriteLine("--------+----------");
